7108986919bec239e4382f5d272cc0ad92744c04
vito_de_huli <vitoDeHuli@gmail.com>
2019-08-26 21:45:30 +0300

    add naming for routes

--- server/app/routes.py
+++ server/app/routes.py
@@ -2,6 +2,6 @@ import views
 
 
 def setup_routes(app):
-    app.router.add_view('/', views.IndexView)
-    app.router.add_view('/messages', views.MessageView)
-    app.router.add_view(r'/message/{pk:\d+}', views.MessageDetailView)
+    app.router.add_view('/', views.IndexView, name='index')
+    app.router.add_view('/messages', views.MessageView, name='message_list')
+    app.router.add_view(r'/message/{pk:\d+}', views.MessageDetailView, name='message_detail')
